d83bbcccfcc990639ab3ed6551c695d171e9315c
Alex Bae <alexbae84@gmail.com>
2020-06-06 19:45:43 -0700

    add home

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,13 @@ import React from 'react'
 import '../styles/Home.css'
 
 import User from './User'
+import { Link } from 'react-router-dom'
 
 const Main = ({ user, children }) => (
     <div>
         <header className="shop-topbar">
             <div>
-                <span>Logo</span>
+                <Link to={'/'}>Logo</Link>
             </div>
             <User user={user} />
         </header>
